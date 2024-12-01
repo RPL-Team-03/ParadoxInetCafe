cfa50bdce886707d87f81885fcda7ea4c491ddeb
lonewanderer69 <ahmadraihan690@gmail.com>
2024-12-01 22:31:32 +0700

    landing page
    rapiin dikit ram miripin di figma

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="w-full h-24 flex items-center justify-between font-Quicksand bg-black">
+    <div class="pl-14">
+        <a href="#" class="items-center space-x-4">
+            <span class="text-2xl font-bold text-white">PARADOX GAME CENTER</span>
+        </a>
+    </div>
+    <div class="flex items-center space-x-10">
+        <div class="flex py-1 space-x-6">
+            <a href="#" class="text-lg font-medium text-white hover:text-blue-300">HOME</a>
+            <a href="#" class="text-lg font-medium text-white hover:text-blue-300">NEWS</a>
+            <a href="#" class="text-lg font-medium text-white hover:text-blue-300">REVIEW</a>
+            <a href="#" class="text-lg font-medium text-white hover:text-blue-300">FAQ</a>
+            <a href="#" class="text-lg font-medium text-white hover:text-blue-300">ABOUT</a>
+        </div>
+    </div>
+    <div class="text-lg font-medium px-16">
+        <div class="bg-red-500 hover:bg-red-400 text-white text-lg font-medium square-full px-6 py-2">
+            <a href="#" class="text-white">LOGIN</a>
+        </div>
+    </div>
+</nav>
