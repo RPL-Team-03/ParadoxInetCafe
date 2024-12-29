2ea2e824ed77da6f1c95f4cc10f728445b320f5a
Alif Rizki Ramdhana <alifrizki1011@gmail.com>
2024-12-29 16:27:22 +0700

    update: vvip new

--- resources/views/vvip.blade.php
+++ resources/views/vvip.blade.php
@@ -9,15 +9,17 @@
         <div class="flex items-center justify-center pt-14">
             <img src="assets/img/VVIP_Detail.png" alt="PC1" class="w-3/4 mt-6 ml-72 rounded-md"/>
         </div>
-        <div class="text-white mt-20">
-            <div class="flex justify-center text-center text-4xl">
-                <h1 class="font-extrabold">VVIP SPECIFICATION</h1>
-            </div>
-            <div class="space-y-5 justify-center text-center mt-12">
-                <p class="text-3xl font-semibold">NVIDIA GTX 1660 Ti</p>
-                <p class="text-3xl font-semibold">Intel Core i3-10100F</p>
-                <p class="text-3xl font-semibold">Monitor Zowie 144Hz</p>
-                <p class="text-3xl font-semibold">RAM 16GB</p>
+        <div class="flex justify-center pl-20">
+            <div class="text-white py-20 px-10">
+                <div class="flex justify-center text-center text-4xl">
+                    <h1 class="font-extrabold">VVIP SPECIFICATION</h1>
+                </div>
+                <div class="space-y-5 justify-center text-center mt-12">
+                    <p class="text-3xl font-semibold">NVIDIA GTX 1660 Ti</p>
+                    <p class="text-3xl font-semibold">Intel Core i3-10100F</p>
+                    <p class="text-3xl font-semibold">Monitor Zowie 144Hz</p>
+                    <p class="text-3xl font-semibold">RAM 16GB</p>
+                </div>
             </div>
         </div>
     </div>
